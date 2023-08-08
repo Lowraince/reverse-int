cd734e41aa53705bf721a2c7461206fdea47a779
Lowraince <besseion@yandex.ru>
2023-08-08 12:54:37 +0300

    init: end task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    const toString = n.toString()
+    const toArray = [...toString]
+
+    console.log(toArray)
+
+    if(toArray[0] === "-"){
+        return toArray.slice(1).reverse().join("")
+    }else
+        return toArray.reverse().join("")
 }
