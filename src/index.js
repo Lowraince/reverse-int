696767d045b4d5d1324838504ba6f29fc48353d7
Lowraince <besseion@yandex.ru>
2023-08-08 13:08:32 +0300

    refactor: delete console.log

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ module.exports = function reverse (n) {
     const toString = n.toString()
     const toArray = [...toString]
 
-    console.log(toArray)
-
     if(toArray[0] === "-"){
         return toArray.slice(1).reverse().join("")
     }else
